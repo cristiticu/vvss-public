e8c9125f50c38ac73110f778fb5ee8f9ec9ec3e0
Cristian Ticu <cristian.ticu@eggnita.com>
2025-04-10 17:23:35 +0300

    Add lab3 stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,9 +5,20 @@ module pizzashop {
     requires java.desktop;
 
     opens pizzashop.model to javafx.base;
+
     exports pizzashop.model;
+
     opens pizzashop to javafx.fxml;
+
     exports pizzashop;
+
     opens pizzashop.controller to javafx.fxml;
+
     exports pizzashop.controller;
+
+    exports pizzashop.service;
+    exports pizzashop.repository;
+
+    // opens pizzashop.repository to org.junit.platform.commons;
+    opens pizzashop.repository;
 }
---
+++ src/test/java/pizzashop/service/PizzaServiceTest.java
@@ -0,0 +1,110 @@
+package pizzashop.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import pizzashop.model.Payment;
+import pizzashop.model.PaymentType;
+import pizzashop.repository.MenuRepository;
+import pizzashop.repository.PaymentRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class PizzaServiceTest {
+    @Mock
+    private PaymentRepository paymentRepository;
+
+    @Mock
+    private MenuRepository menuRepository;
+
+    @InjectMocks
+    private PizzaService pizzaService;
+
+    @Test
+    @DisplayName("Empty payment list")
+    public void getTotalEmpty() {
+        when(paymentRepository.getAll()).thenReturn(Collections.emptyList());
+
+        double result = pizzaService.getTotalAmount(PaymentType.Card);
+        assertEquals(0.0, result);
+
+        when(paymentRepository.getAll()).thenReturn(null);
+
+        result = pizzaService.getTotalAmount(PaymentType.Card);
+        assertEquals(0.0, result);
+    }
+
+    @Test
+    @DisplayName("One matching payment")
+    public void getTotalOnePayment() {
+        List<Payment> payments = List.of(new Payment(1, PaymentType.Card, 50.0));
+        when(paymentRepository.getAll()).thenReturn(payments);
+
+        double result = pizzaService.getTotalAmount(PaymentType.Card);
+        assertEquals(50.0, result);
+    }
+
+    @Test
+    @DisplayName("One non-matching payment")
+    public void getTotalOneNonMatchingPayment() {
+        List<Payment> payments = List.of(new Payment(1, PaymentType.Cash, 100000.0));
+        when(paymentRepository.getAll()).thenReturn(payments);
+
+        double result = pizzaService.getTotalAmount(PaymentType.Card);
+        assertEquals(0.0, result);
+    }
+
+    @Test
+    @DisplayName("Two matching payments")
+    public void getTotalTwoPayments() {
+        List<Payment> payments = List.of(
+                new Payment(1, PaymentType.Cash, 40.0),
+                new Payment(1, PaymentType.Cash, 50.0));
+
+        when(paymentRepository.getAll()).thenReturn(payments);
+
+        double result = pizzaService.getTotalAmount(PaymentType.Cash);
+        assertEquals(90.0, result);
+    }
+
+    @Test
+    @DisplayName("Two non-matching payments")
+    public void getTotalTwoNonMatchingPayments() {
+        List<Payment> payments = List.of(
+                new Payment(1, PaymentType.Cash, 10000000.0),
+                new Payment(1, PaymentType.Cash, 1000000.0));
+
+        when(paymentRepository.getAll()).thenReturn(payments);
+
+        double result = pizzaService.getTotalAmount(PaymentType.Card);
+        assertEquals(0.0, result);
+    }
+
+    @Test
+    @DisplayName("Mixed payments")
+    public void getTotalMixedPayments() {
+        List<Payment> payments = List.of(
+                new Payment(1, PaymentType.Cash, 10000.0),
+                new Payment(1, PaymentType.Card, 50.0),
+                new Payment(1, PaymentType.Card, 10.0),
+                new Payment(1, PaymentType.Cash, 10000.0));
+
+        when(paymentRepository.getAll()).thenReturn(payments);
+
+        double result = pizzaService.getTotalAmount(PaymentType.Card);
+        assertEquals(60.0, result);
+    }
+}
